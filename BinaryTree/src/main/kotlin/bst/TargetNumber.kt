41dba283521304ae489f8af3120314b95687b4ec
rockpell <rockpell.cleaning@gmail.com>
2022-04-30 20:30:19 +0900

    Feat: 타겟 넘버 문제

---
+++ BinaryTree/src/main/kotlin/bst/TargetNumber.kt
@@ -0,0 +1,16 @@
+package bst
+
+class TargetNumber {
+    fun solution(numbers: IntArray, target: Int): Int {
+        return targetNumberRecursive(target, 0, numbers, 0)
+    }
+
+    private fun targetNumberRecursive(target: Int, sum: Int, numbers: IntArray, index: Int): Int {
+        if (target == sum && numbers.size == index)
+            return 1
+        if (index >= numbers.size)
+            return 0
+        return targetNumberRecursive(target, sum + numbers[index], numbers, index + 1) +
+                targetNumberRecursive(target, sum - numbers[index], numbers, index + 1)
+    }
+}
---
+++ BinaryTree/src/test/kotlin/bst/TargetNumberTest.kt
@@ -0,0 +1,18 @@
+package bst
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class TargetNumberTest {
+
+    @Test
+    fun solution() {
+        val numbers = intArrayOf(1, 1, 1, 1, 1)
+        val target = 3
+        val targetNumber = TargetNumber()
+
+        assertEquals(5, targetNumber.solution(numbers, target))
+    }
+
+}
